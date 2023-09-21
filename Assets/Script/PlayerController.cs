f7b7906b161b0ab7ddec557e711389f7340191a1
kaito913ps <kaito913jp@gmail.com>
2023-09-21 23:01:32 +0900

    一旦PlayerControllerを作成した

--- Assets/Script/PlayerController.cs
+++ Assets/Script/PlayerController.cs
@@ -4,15 +4,30 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
+    [SerializeField,Tooltip("e’e‚ÌƒvƒŒƒnƒu")]
+    private GameObject _bulletPrefab;
+    [SerializeField,Tooltip("e’e‚Ì”­ËˆÊ’u")]
+    private Transform _firePoint;
+    [SerializeField,Tooltip("e’e‚Ì‘¬“x")]
+    private float _bulletSpeed = 10f;
+
     void Update()
     {
-        
+        //ƒ{ƒ^ƒ“‚ª‰Ÿ‚³‚ê‚½‚çe’e‚ğ”­Ë
+        if(Input.GetButtonDown("Fire1"))
+        {
+            Shoot();
+        }
+    }
+
+    void Shoot()
+    {
+        //e’e‚ğ¶¬‚µA”­ËˆÊ’u‚É”z’u
+        GameObject bullet = Instantiate(_bulletPrefab, _firePoint.position, _firePoint.rotation);
+
+        //e’e‚É‘¬“x‚ğİ’è
+        Rigidbody2D rb = bullet.GetComponent<Rigidbody2D>();
+        rb.velocity = transform.up * _bulletSpeed;
     }
 }
